9bf996160848464e8b0bd0980e00bed52a54b96d
Haidar Zamzam <haidar.zamzam99@gmail.com>
2020-04-17 11:18:15 +0700

    Add Detail TableView

---
+++ VacationSpotApp/Controller/DetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DetailViewController.swift
+//  VacationSpotApp
+//
+//  Created by Haidar Zamzam on 17/04/20.
+//  Copyright © 2020 Haidar Zamzam. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var nameVacation: UILabel!
+    @IBOutlet weak var photoVacation: UIImageView!
+    @IBOutlet weak var descVacation: UILabel!
+    
+    var vacation: Vacation?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let result = vacation {
+            nameVacation.text = result.name
+            photoVacation.image = result.photo
+            descVacation.text = result.desc
+            
+            // Kode ini digunakan untuk membuat imageView memiliki frame bound/lingkaran
+            photoVacation.layer.cornerRadius = photoVacation.frame.height / 4
+            photoVacation.contentMode = .scaleAspectFill
+        }
+        
+    }
+
+}
--- VacationSpotApp/Controller/ViewController.swift
+++ VacationSpotApp/Controller/ViewController.swift
@@ -14,11 +14,23 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-         // Menghubungkan vacationTableView dengan ke dua metode di bawah
-           vacationTableView.dataSource = self
-               
-           // Menghubungkan berkas XIB untuk HeroTableViewCell dengn heroTableView.
-           vacationTableView.register(UINib(nibName: "VacationTableViewCell", bundle: nil), forCellReuseIdentifier: "VacationCell")
+        // Menambahkan title pada Navigation
+        self.navigationItem.title = "Wisata Indonesia"
+        
+        
+        let aboutButton = UIButton(type: .system)
+        aboutButton.setImage(#imageLiteral(resourceName: "user-circle").withRenderingMode(.alwaysOriginal), for: .normal)
+        aboutButton.frame = CGRect(x:0, y:0, width:34, height:34)
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: aboutButton)
+        // Menghubungkan vacationTableView dengan ke dua metode di bawah
+        vacationTableView.dataSource = self
+            
+        // Menghubungkan vacationTableView dengan ke dua metode di bawah
+        vacationTableView.delegate = self
+        
+        // Menghubungkan berkas XIB untuk HeroTableViewCell dengn heroTableView.
+        vacationTableView.register(UINib(nibName: "VacationTableViewCell", bundle: nil), forCellReuseIdentifier: "VacationCell")
     }
 }
 
@@ -42,5 +54,18 @@ extension ViewController: UITableViewDataSource{
         cell.photoVacation.contentMode = .scaleAspectFill
         
         return cell
+    }
+}
+
+extension ViewController:UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Memanggil View Controller dengan berkas XIB di dalamnya
+        let detail = DetailViewController(nibName: "DetailViewController", bundle: nil)
+        
+        // Mengirim data vacation
+        detail.vacation = vacations[indexPath.row]
+        
+        // Push/mendorong view controller lain
+        self.navigationController?.pushViewController(detail, animated: true)
     }
 }
